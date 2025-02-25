50504c0b0dca033e6f93700e37863b574be64f20
nahyeon111 <na00hyeon@gmail.com>
2025-02-25 13:36:18 +0900

    yaw 추가

--- smart_imu/angular_accelation_ya_ro_pi.py
+++ smart_imu/angular_accelation_ya_ro_pi.py
@@ -1,4 +1,3 @@
-
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Imu
@@ -12,18 +11,18 @@ class IMUOrientationCalculator(Node):
         # ✅ 자이로 데이터 구독
         self.imu_sub = self.create_subscription(Imu, '/android/imu', self.imu_callback, 10)
 
-        # ✅ 계산된 roll, pitch 발행
+        # ✅ 계산된 roll, pitch, yaw 발행
         self.orientation_pub = self.create_publisher(Vector3, '/imu/orientation', 10)
 
         # ✅ 초기값 설정
         self.prev_time = None
         self.roll = 0.0  # Roll (x축 회전)
         self.pitch = 0.0  # Pitch (y축 회전)
+        self.yaw = 0.0 # yaw (z축 회전)
 
         self.get_logger().info("IMU Orientation Calculator Node Started!")
 
     def imu_callback(self, msg):
-        """ 자이로스코프 데이터를 적분하여 roll, pitch 계산 """
         current_time = self.get_clock().now().nanoseconds / 1e9  # 초 단위 변환
 
         # 첫 데이터 수신 시 시간 초기화
@@ -31,31 +30,35 @@ class IMUOrientationCalculator(Node):
             self.prev_time = current_time
             return
 
-        # ✅ 1. 자이로스코프 데이터 가져오기 (rad/s)
+        # 자이로스코프 데이터 가져오기 (rad/s)
         gyro_x = msg.angular_velocity.x
         gyro_y = msg.angular_velocity.y
+        gyro_z = msg.angular_velocity.z
 
-        # ✅ 2. 시간 변화량 계산
+        # 시간 변화량 계산
         dt = current_time - self.prev_time
         self.prev_time = current_time
 
-        # ✅ 3. Roll, Pitch 적분 (단순 오일러 적분)
+        # Roll, Pitch 적분 (단순 오일러 적분)
         self.roll += gyro_x * dt
         self.pitch += gyro_y * dt
+        self.yaw += gyro_z * dt 
 
-        # ✅ 4. Roll, Pitch 값 발행
+        # roll, Pitch, yaw 값 발행 
         orientation_msg = Vector3()
         orientation_msg.x = np.degrees(self.roll)  # 라디안을 도(degree)로 변환
         orientation_msg.y = np.degrees(self.pitch)
-        orientation_msg.z = 0.0  # Yaw는 계산하지 않음
+        orientation_msg.z = np.degrees(self.yaw)
 
         self.orientation_pub.publish(orientation_msg)
 
         # ✅ 5. 디버깅 로그 출력
         self.get_logger().info(f"""
-📌 Gyro Data (rad/s): x={gyro_x:.3f}, y={gyro_y:.3f}
-🕒 Time Step: {dt:.3f}s
-🎯 Computed Orientation (deg): Roll={orientation_msg.x:.3f}, Pitch={orientation_msg.y:.3f}
+📌Gyro Data (rad/s): 
+    x={gyro_x:.3f}, y={gyro_y:.3f}, z={gyro_z:.3f}
+📌Computed Orientation (deg): 
+    Roll={orientation_msg.x:.3f}, Pitch={orientation_msg.y:.3f}, Yaw={orientation_msg.z:.3f}
+📌Time Step: {dt:.3f}s
         """)
 
 def main():
